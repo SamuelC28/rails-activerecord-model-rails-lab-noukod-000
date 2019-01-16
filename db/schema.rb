b8a37b9bcf3ad19fbf5641cb3b3d38d46e09049e
Samuel Cornet <samuel@noukod.com>
2019-01-16 20:31:36 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,5 +12,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+  create_table "users", force: :cascade do |t|
+    t.string :first_name
+    t.string :last_name
+  end
 
 end
