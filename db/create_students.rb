741259b66316941c4c2d4ca8274e993df185dc55
Samuel Cornet <samuel@noukod.com>
2019-01-16 20:26:23 +0000

    Automatically backed up by Learn

--- db/create_students.rb
+++ db/create_students.rb
@@ -1,8 +1,7 @@
 class Student < ActiveRecord::Migration[5.1]
   def change
     create_table :students do |t|
-      t.string :username
-     t.string :password
-     t.decimal :balance
-   end 
+      t.string :first_name
+      t.string :last_name
+    end
 end
